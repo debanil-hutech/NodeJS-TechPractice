0697cfbf557ef879eb172006c0057c2deb06844e
Debanil Guha <debanil@hutechsolutions.com>
2022-04-16 10:31:37 +0530

    Added changes

--- app.js
+++ app.js
@@ -4,7 +4,28 @@ const bodyParser=require('body-parser');
 
 app.use(bodyParser.json());
 
-app.get('/',(req,res)=>{
+const users = [
+    {id: 'debanil', name: 'Debanil'},
+    {id: 'swaroop', name: 'Swaroop'},
+    {id: 'vimarsh', name: 'Vimarsh'},
+    {id: 'madan', name: 'Madan'},
+    {id: 'pankaj', name: 'Pankaj'},
+];
+
+const actors = [
+    {id: '1', name: 'Dhanush'},
+    {id: '2', name: 'Rajni'},
+    {id: '3', name: 'Amir'},
+];
+
+const movieActor = [
+    {id: '1', name: 'Movie A'},
+    {id: '3', name: 'Movie A'},
+    {id: '2', name: 'Movie B'},
+    {id: '1', name: 'Movie C'},
+]
+
+app.get('/userid/actor',(req,res)=>{
     console.log(req.body);
 })
 
